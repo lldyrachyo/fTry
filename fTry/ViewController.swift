098bf5315f3b00a7b7fbbf5270acd1efcaf650df
Mark <papushman@yandex.ru>
2023-03-17 16:08:37 +0300

    Initial Commit

---
+++ fTry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fTry
+//
+//  Created by Mark Papush on 3/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
